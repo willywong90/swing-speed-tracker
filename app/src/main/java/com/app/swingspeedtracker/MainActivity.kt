9a6ff95556fdf08fec8b09b863f8868422719d18
Willy W <will.wong90@gmail.com>
2022-12-27 14:51:54 -0800

    Refactor to use a ViewModel

--- app/src/main/java/com/app/swingspeedtracker/MainActivity.kt
+++ app/src/main/java/com/app/swingspeedtracker/MainActivity.kt
@@ -1,50 +1,29 @@
 package com.app.swingspeedtracker
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.bluetooth.*
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanFilter
-import android.bluetooth.le.ScanResult
-import android.bluetooth.le.ScanSettings
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.os.ParcelUuid
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
-import com.app.swingspeedtracker.ui.theme.SwingSpeedTrackerTheme
+import com.app.swingspeedtracker.ui.screens.MainScreen
 import java.util.*
-import kotlin.collections.ArrayList
 
 private const val TAG = "SwingSpeedTracker"
-private val UUID_GST_7_SERVICE = UUID.fromString("dc030000-7c54-44fa-bca6-c61732a248ef")
-private val UUID_SENSOR_CHARACTERISTIC = UUID.fromString("dc030001-7c54-44fa-bca6-c61732a248ef")
-private val UUID_SENSOR_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
 
 @RequiresApi(Build.VERSION_CODES.Q)
 class MainActivity : ComponentActivity() {
-    private var isBluetoothConnected by mutableStateOf(false)
-    private var sensorData = mutableStateListOf<TrackerData>()
     private lateinit var getPermission: ActivityResultLauncher<String>
     private lateinit var getBluetoothIntentResult: ActivityResultLauncher<Intent>
-    private lateinit var bluetoothSensor: BluetoothDevice
 
     private val isBluetoothPermissionGranted
         get() = hasPermission(Manifest.permission.BLUETOOTH_CONNECT)
@@ -57,6 +36,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        checkBluetoothPermissions()
+        initializeBluetooth()
+
+        setContent {
+            MainScreen()
+        }
+    }
+
+    private fun checkBluetoothPermissions() {
         getPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { hasPermission ->
             if (!hasPermission) {
                 Toast.makeText(applicationContext, "No permission to perform this action", Toast.LENGTH_SHORT)
@@ -74,11 +62,12 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        initializeBluetooth()
-        scanForSensor()
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            getPermission.launch(Manifest.permission.BLUETOOTH_SCAN)
+        }
 
-        setContent {
-            MainView(isBluetoothConnected, sensorData)
+        if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+            getPermission.launch(Manifest.permission.BLUETOOTH_CONNECT)
         }
     }
 
@@ -92,154 +81,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private fun scanForSensor() {
-        val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
-        val scanFilters: MutableList<ScanFilter> = ArrayList()
-        val scanSettings = ScanSettings.Builder()
-            .setCallbackType(ScanSettings.CALLBACK_TYPE_FIRST_MATCH)
-            .build()
-        val nameFilter = ScanFilter.Builder()
-            .setServiceUuid(ParcelUuid(UUID_GST_7_SERVICE))
-            .build()
-
-        scanFilters.add(nameFilter)
-
-        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
-            getPermission.launch(Manifest.permission.BLUETOOTH_SCAN)
-        }
-
-        if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
-            getPermission.launch(Manifest.permission.BLUETOOTH_CONNECT)
-        }
-
-        Log.d(TAG, "STARTING BLUETOOTH SCAN")
-        bluetoothLeScanner.startScan(scanFilters, scanSettings, bluetoothLeScanCallback)
-    }
-
-    @SuppressLint("MissingPermission")
-    private val bluetoothLeScanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-
-            Log.d(TAG, "FOUND DEVICE " + result.device.address)
-
-            bluetoothSensor = bluetoothAdapter.getRemoteDevice(result.device.address)
-            bluetoothSensor.connectGatt(applicationContext, false, bluetoothGattCallback, BluetoothDevice.TRANSPORT_LE)
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private val bluetoothGattCallback = object : BluetoothGattCallback() {
-        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-                if (newState == BluetoothProfile.STATE_CONNECTED) {
-                    // successfully connected to the GATT Server
-                    Log.d(TAG, "CONNECTED TO GATT SERVER")
-                    isBluetoothConnected = true
-                    gatt?.discoverServices()
-                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                    Log.e(TAG, "DISCONNECTED FROM GATT SERVER")
-                    isBluetoothConnected = false
-                    gatt?.close()
-                }
-            } else {
-                Log.e(TAG, "DISCONNECTED FROM GATT SERVER")
-                isBluetoothConnected = false
-                gatt?.close()
-            }
-        }
-
-        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-                val sensorService = gatt?.getService(UUID_GST_7_SERVICE)
-                val sensorCharacteristic = sensorService?.getCharacteristic(UUID_SENSOR_CHARACTERISTIC)
-                if (sensorCharacteristic != null) {
-                    gatt.setCharacteristicNotification(sensorCharacteristic, true)
-                    val descriptor = sensorCharacteristic.getDescriptor(UUID_SENSOR_CHARACTERISTIC_CONFIG)
-                    descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
-                    gatt.writeDescriptor(descriptor)
-                }
-            } else {
-                Log.e(TAG, "Service discovery failed")
-                isBluetoothConnected = false
-                gatt?.close()
-            }
-        }
-
-        override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
-            super.onCharacteristicChanged(gatt, characteristic)
-
-            if (characteristic?.uuid.toString() == UUID_SENSOR_CHARACTERISTIC.toString()) {
-                val data: ByteArray? = characteristic?.value
-
-                if (data?.isNotEmpty() == true && data?.size > 4) {
-                    val hexString: String = data.joinToString(separator = " ") {
-                        String.format("%02X", it)
-                    }
-                    Log.d(TAG, "DATA RECEIVED: $hexString")
-                    sensorData.add(0, convertData(data))
-                }
-            }
-        }
-    }
-
-    private fun convertData(data: ByteArray): TrackerData {
-        val hexString: String = data.joinToString(separator = " ") {
-            String.format("%02X", it)
-        }
-        val hexList = hexString.split(" ").reversed()
-
-        val clubHeadSpeed = hexToDec(hexList.subList(12,14)).toDouble()/10
-        val ballSpeed = hexToDec(hexList.subList(10,12)).toDouble()/10
-        val distance = hexToDec(hexList.subList(7,9)).toDouble()
-        val club = hexList[9].toInt(16)
-
-        return TrackerData(clubHeadSpeed, ballSpeed, distance, club)
-    }
-
-    private fun hexToDec(hex: List<String>): Int {
-        return hex.joinToString(separator = "").toInt(16)
-    }
-
     private fun hasPermission(permissionType: String): Boolean {
         return ActivityCompat.checkSelfPermission(this, permissionType) == PackageManager.PERMISSION_GRANTED
     }
-}
-
-@Composable
-fun MainView(bluetoothStatus: Boolean, dataList: MutableList<TrackerData>) {
-    var btStatusColor = Color.DarkGray
-    if (bluetoothStatus)
-        btStatusColor = Color.Blue
-
-    val displayData = if (dataList.size > 0) dataList[0] else null
-
-    SwingSpeedTrackerTheme {
-        Surface (
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Column {
-                Box(modifier = Modifier
-                    .fillMaxWidth()
-                    .height(2.dp)
-                    .background(btStatusColor)
-                )
-                TrackerDisplay(displayData)
-                TrackerList(dataList)
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    val testDisplay = TrackerData(11.11, 22.22, 33.33, 0)
-    val testList = remember {mutableStateListOf<TrackerData>()}
-    testList.add(testDisplay)
-    testList.add(testDisplay)
-    testList.add(testDisplay)
-
-    MainView(true, testList)
 }
---
+++ app/src/main/java/com/app/swingspeedtracker/MainViewModel.kt
@@ -0,0 +1,172 @@
+package com.app.swingspeedtracker
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.bluetooth.*
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.os.Build
+import android.os.ParcelUuid
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.*
+import androidx.lifecycle.AndroidViewModel
+import java.util.*
+import kotlin.collections.ArrayList
+
+private const val TAG = "SwingSpeedTracker"
+private val UUID_GST_7_SERVICE = UUID.fromString("dc030000-7c54-44fa-bca6-c61732a248ef")
+private val UUID_SENSOR_CHARACTERISTIC = UUID.fromString("dc030001-7c54-44fa-bca6-c61732a248ef")
+private val UUID_SENSOR_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+
+@RequiresApi(Build.VERSION_CODES.M)
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val applicationContext = getApplication<Application>().applicationContext
+    private var isBluetoothOn by mutableStateOf(false)
+    private var bluetoothLeScanCallback: ScanCallback
+    private lateinit var bluetoothSensor: BluetoothDevice
+    private lateinit var bluetoothGattCallback: BluetoothGattCallback
+    val sensorData = mutableStateListOf<TrackerData>()
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = applicationContext.getSystemService(ComponentActivity.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun scanForSensor() {
+        val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        val scanFilters: MutableList<ScanFilter> = ArrayList()
+        val scanSettings = ScanSettings.Builder()
+            .setCallbackType(ScanSettings.CALLBACK_TYPE_FIRST_MATCH)
+            .build()
+        val nameFilter = ScanFilter.Builder()
+            .setServiceUuid(ParcelUuid(UUID_GST_7_SERVICE))
+            .build()
+
+        scanFilters.add(nameFilter)
+        Log.d(TAG, "STARTING BLUETOOTH SCAN")
+        bluetoothLeScanner.startScan(scanFilters, scanSettings, bluetoothLeScanCallback)
+    }
+
+    private fun convertData(data: ByteArray): TrackerData {
+        val hexString: String = data.joinToString(separator = " ") {
+            String.format("%02X", it)
+        }
+        val hexList = hexString.split(" ").reversed()
+
+        val clubHeadSpeed = hexToDec(hexList.subList(12,14)).toDouble()/10
+        val ballSpeed = hexToDec(hexList.subList(10,12)).toDouble()/10
+        val distance = hexToDec(hexList.subList(7,9)).toDouble()
+        val club = hexList[9].toInt(16)
+
+        return TrackerData(clubHeadSpeed, ballSpeed, distance, club)
+    }
+
+    private fun hexToDec(hex: List<String>): Int {
+        return hex.joinToString(separator = "").toInt(16)
+    }
+
+    fun getBluetoothState(): Boolean {
+        return isBluetoothOn
+    }
+
+    fun getAveragedStats(): TrackerData {
+        var averageClubSpeed = 0.0
+        var averageBallSpeed = 0.0
+        var averageCarry = 0.0
+
+        if (sensorData.isNotEmpty()) {
+            sensorData.forEach {
+                averageClubSpeed += it.clubSpeed
+                averageBallSpeed += it.ballSpeed
+                averageCarry += it.carry
+            }
+
+            averageClubSpeed /= sensorData.size
+            averageBallSpeed /= sensorData.size
+            averageCarry /= sensorData.size
+        }
+
+        return TrackerData(averageClubSpeed, averageBallSpeed, averageCarry, 0)
+    }
+
+    fun clearHistory() {
+        sensorData.clear()
+    }
+
+    init {
+        @SuppressLint("MissingPermission")
+        bluetoothLeScanCallback = object: ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                Log.d(TAG, "FOUND DEVICE " + result.device.address)
+
+                bluetoothSensor = bluetoothAdapter.getRemoteDevice(result.device.address)
+                bluetoothSensor.connectGatt(applicationContext, false, bluetoothGattCallback, BluetoothDevice.TRANSPORT_LE)
+            }
+        }
+
+        @SuppressLint("MissingPermission")
+        bluetoothGattCallback = object: BluetoothGattCallback() {
+            override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        Log.d(TAG, "CONNECTED TO GATT SERVER")
+
+                        isBluetoothOn = true
+                        gatt?.discoverServices()
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        Log.e(TAG, "DISCONNECTED FROM GATT SERVER")
+
+                        isBluetoothOn = false
+                        gatt?.close()
+                    }
+                } else {
+                    Log.e(TAG, "DISCONNECTED FROM GATT SERVER")
+
+                    isBluetoothOn = false
+                    gatt?.close()
+                }
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    val sensorService = gatt?.getService(UUID_GST_7_SERVICE)
+                    val sensorCharacteristic = sensorService?.getCharacteristic(UUID_SENSOR_CHARACTERISTIC)
+                    if (sensorCharacteristic != null) {
+                        gatt.setCharacteristicNotification(sensorCharacteristic, true)
+                        val descriptor = sensorCharacteristic.getDescriptor(UUID_SENSOR_CHARACTERISTIC_CONFIG)
+                        descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                        gatt.writeDescriptor(descriptor)
+                    }
+                } else {
+                    Log.e(TAG, "Service discovery failed")
+                    isBluetoothOn = false
+                    gatt?.close()
+                }
+            }
+
+            override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
+                super.onCharacteristicChanged(gatt, characteristic)
+
+                if (characteristic?.uuid.toString() == UUID_SENSOR_CHARACTERISTIC.toString()) {
+                    val data: ByteArray? = characteristic?.value
+
+                    if (data?.size!! > 4) {
+                        val hexString: String = data.joinToString(separator = " ") {
+                            String.format("%02X", it)
+                        }
+                        Log.d(TAG, "DATA RECEIVED: $hexString")
+                        sensorData.add(0, convertData(data))
+                    }
+                }
+            }
+        }
+
+        scanForSensor()
+    }
+}
