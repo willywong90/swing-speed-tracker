79bfee82791174b3420ffcd400c57055e05b9fb8
Willy W <will.wong90@gmail.com>
2023-11-29 15:36:27 -0800

    Add date to each tracker item

--- app/src/main/java/com/app/swingspeedtracker/data/TrackerData.kt
+++ app/src/main/java/com/app/swingspeedtracker/data/TrackerData.kt
@@ -1,14 +1,20 @@
 package com.app.swingspeedtracker.data
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+
 private const val MS_TO_MPH_CONVERSION = 2.23694
 private val CLUB_LABEL = listOf("W1", "W3", "W5", "W7", "W9", "U2", "U3", "U4", "U5", "U6", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "PW", "AW", "Sw", "LW", "PT")
 
+@RequiresApi(Build.VERSION_CODES.O)
 data class TrackerData(
     val clubSpeed: Double,
     val ballSpeed: Double,
     val distance: Double,
     val club: Int,
-    val isSelected: Boolean = false
+    val isSelected: Boolean = false,
+    val date: LocalDateTime = LocalDateTime.now()
 ) {
     val clubSpeedMph: Double = clubSpeed
         get() = field * MS_TO_MPH_CONVERSION
